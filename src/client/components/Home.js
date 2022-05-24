fce12214bf0ec1f7119db954b9bb5d2b9b0ad51d
sone <sthaya@gmail.com>
2022-05-23 20:50:41 -0700

    setup client js

--- src/client/components/Home.js
+++ src/client/components/Home.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Home = () => {
-  return <div>I'm the best very oh yeah Home component</div>;
+  return (
+    <div>
+      <div>I'm the best very oh yeah Home component</div>
+      <button onClick={() => console.log("hello")}>Press me!</button>
+    </div>
+  );
 };
 
 export default Home;
